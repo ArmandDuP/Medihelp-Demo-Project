467d60fa5104f129c856ba6e7beaf6f4a9f2f9b4
arman <arman@ARMAND-ASUS>
2023-03-28 14:30:35 +0200

    Add project files.

---
+++ Demo Project (ASP.NET Core Web API)/Models/UserContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_Project__ASP.NET_Core_Web_API_.Models;
+
+public partial class UserContext : DbContext
+{
+    public UserContext()
+    {
+    }
+
+    public UserContext(DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserDetail> UserDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserDetail>(entity =>
+        {
+            entity.ToTable("UserDetail");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Demo Project (ASP.NET Core Web API)/Models/UserDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_Project__ASP.NET_Core_Web_API_.Models;
+
+public partial class UserDetail
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
